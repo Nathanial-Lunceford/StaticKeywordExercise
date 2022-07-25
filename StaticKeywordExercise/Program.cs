59dfd55280fa5d8a2a302540c68aba958a1760a7
Nate <nathanial.lunceford@gmail.com>
2022-07-25 10:53:39 -0500

    Initial Commit

---
+++ StaticKeywordExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using StaticKeywordExercise;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine($"40 Fahrenheit is {TempConverter.FahrenheitToCelcius(40)} Celsius, and " +
+            $"32.22 Celsius is {TempConverter.CelciusToFahrenheit(32.22)} Fahrenheit");
+    }
+}
---
+++ StaticKeywordExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticKeywordExercise
+{
+    static public class TempConverter
+    {
+        static public double FahrenheitToCelcius(double fTemp)
+        {
+            return (fTemp - 32) * 5/9;
+        }
+
+        static public double CelciusToFahrenheit(double cTemp)
+        {
+            return cTemp * 9/5 + 32;
+        }
+    }
+}
